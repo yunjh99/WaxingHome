fac5fbedbb4f84fe88217d7bb9e8bbd4199dab21
orenjh1219 <yunjh1219@gmail.com>
2025-12-04 21:22:16 +0900

    first commit

---
+++ src/main/java/com/example/waxingweb/WaxingWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.waxingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaxingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaxingWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/waxingweb/WaxingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waxingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaxingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
