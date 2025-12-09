b42e7360b233b9f90f39e3000f047ae416b34df6
yunjh99 <yunj29725@gmail.com>
2025-12-09 15:39:10 +0900

    기본 폴더 구조 생성

--- src/main/java/com/example/waxingweb/common/controller/MainController.java
+++ src/main/java/com/example/waxingweb/common/controller/MainController.java
@@ -1,4 +1,11 @@
 package com.example.waxingweb.common.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/")
+    public String main() { return "pages/home/index"; }
 }
