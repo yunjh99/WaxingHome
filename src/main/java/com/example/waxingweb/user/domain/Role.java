8042be6a2d5f47fbde6202970abcb063c6dab1be
yunjh99 <yunj29725@gmail.com>
2026-01-06 21:16:29 +0900

    feat : add user domain

--- src/main/java/com/example/waxingweb/user/domain/Role.java
+++ src/main/java/com/example/waxingweb/user/domain/Role.java
@@ -1,4 +1,23 @@
 package com.example.waxingweb.user.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
+
+    ADMIN("ADMIN"),
+    USER("USER");
+
+    private final String key;
+
+    public static Role of(String value) {
+        for (Role role : values()) {
+            if (role.key.equals(value)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("유효하지 않은 Role 값입니다: " + value);
+    }
 }
--- src/main/java/com/example/waxingweb/user/domain/User.java
+++ src/main/java/com/example/waxingweb/user/domain/User.java
@@ -1,6 +1,7 @@
 package com.example.waxingweb.user.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,5 +15,33 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username; //아이디
+
+    @Column(nullable = false)
+    private String password; //비번
+
+    @Column(nullable = false)
+    private String name;     //성함
+
+    @Column(nullable = false, unique = true)
+    private String email;    //이메일
+
+    @Column(nullable = false, unique = true)
+    private String phone;    //폰번호
+
+    @Column(nullable = false)
+    private String address;  //주소
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @PrePersist
+    public void setDefaultRole() {
+        if (this.role == null) {
+            this.role = Role.USER; //
+        }
+    }
 
 }
